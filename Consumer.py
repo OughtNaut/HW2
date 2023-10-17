ea9dfe0a86b947637c7e8fa51345f1f247165431
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-10-17 14:26:16 -0600

    Added python argparser with some proof of concept code

--- Consumer.py
+++ Consumer.py
@@ -1,2 +1,20 @@
 import argparse
 
+
+def consume():
+    parser = argparse.ArgumentParser(
+        prog='Consumer.py',
+        description='Consumes Widget requests from given s3 bucket',
+        epilog=''
+    )
+    parser.add_argument('-rb', '--read_bucket')
+    parser.add_argument('-wb', '--write_bucket')
+    parser.add_argument('-wt', '--write_table')
+
+    args = parser.parse_args()
+
+    print(args.read_bucket)
+
+
+if __name__ == '__main__':
+    consume()
