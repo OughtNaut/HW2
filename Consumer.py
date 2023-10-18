68779d078358092e58c8c193d30eaa210e8ee642
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-10-18 17:52:46 -0600

    Added functionality to request_controller() that sends the request to different functions depending on args. created functions for saving to s3 or dynamodb

--- Consumer.py
+++ Consumer.py
@@ -21,7 +21,7 @@ def consume():
     while True:
         request_key = get_single_key(s3, args)
         if request_key is not None:
-            process_request(request_key, args)
+            process_request(s3, request_key, args)
         else:
             time.sleep(.1)
 
@@ -39,18 +39,46 @@ def process_request(client, request_key, args):
         Bucket=args.read_bucket,
         Key=request_key
     )
-    request_controller(request)
+    request_controller(client, request, args)
     client.delete_object(
         Bucket=args.read_bucket,
         Key=request_key
     )
 
 
-def request_controller(request):
-    pass
+def request_controller(client, request, args):
+    # print(type(request))
+    # request = json.loads(request)
+    request_type = request.get('type')
+    if request_type == 'create':
+        if args.write_bucket is not None:
+            create_widget_s3(client, request, args)
+        if args.write_table is not None:
+            create_widget_dynamo(request, args)
+    if request_type == "delete":
+        # Todo handle delete requests in future
+        return
+    if request_type == "update":
+        # Todo handle update requests in future
+        return
 
-class WidgetFactory():
-    def create_widget(self, request):
-        request = json.loads(request)
+
+def create_widget_dynamo(request_dictionary, args):
+    # I modified this from a code snippet found here https://medium.com/@NotSoCoolCoder/handling-json-data-for-dynamodb-using-python-6bbd19ee884e
+    boto3.resource('dynamodb').Table(args.write_table).put_item(request_dictionary)
+
+
+def create_widget_s3(client, request_dictionary, args):
+    owner = request_dictionary.get('owner').lower().replace(' ', '-')
+    widget_id = request_dictionary.get('widgetId')
+    request_json = json.dumps(request_dictionary)
+    client.put_object(
+        Body=request_json,
+        Bucket=args.write_bucket,
+        Key=f"widgets/{owner}/{widget_id}"
+    )
+
+
+
 if __name__ == '__main__':
     consume()
