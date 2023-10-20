4afb68c9728891dcb9a1ab6e12770aeb1bae7e8c
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-10-20 17:33:28 -0600

    Reworked the logger to have module wide scope. Also added handling of keyboard interupts.

--- Consumer.py
+++ Consumer.py
@@ -1,11 +1,21 @@
 import argparse
+import sys
+
 import boto3
 import time
 import json
 import logging
 
 
+logger = logging.getLogger('consumerLogger')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler = logging.FileHandler('consumer.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 def consume():
+
     parser = argparse.ArgumentParser(
         prog='Consumer.py',
         description='Consumes Widget requests from given s3 bucket',
@@ -18,13 +28,18 @@ def consume():
     args = parser.parse_args()
 
     s3 = boto3.client('s3')
-
-    while True:
-        request_key = get_single_key(s3, args)
-        if request_key is not None:
-            process_request(s3, request_key, args)
-        else:
-            time.sleep(.1)
+    logger.debug("Program Start")
+    try:
+        while True:
+            request_key = get_single_key(s3, args)
+            if request_key is not None:
+                logger.debug(f"found key {request_key} in bucket {args.read_bucket}")
+                process_request(s3, request_key, args)
+            else:
+                time.sleep(.1)
+    except KeyboardInterrupt:
+        logger.debug("Program exited on keyboard interrupt")
+        sys.exit(0)
 
 
 def get_single_key(client, args):
@@ -34,7 +49,9 @@ def get_single_key(client, args):
     )
     contents = response.get('Contents')
     if contents is not None:
+        logger.debug("Retrieved key from AWS s3")
         return contents[0].get('Key')
+    logger.debug("No objects found in s3 bucket.")
     return None
 
 
@@ -48,6 +65,7 @@ def process_request(client, request_key, args):
         Bucket=args.read_bucket,
         Key=request_key
     )
+    logger.debug(f"Request from bucket {args.read_bucket} retrieved and deleted.")
 
 
 def request_controller(client, request, args):
@@ -78,6 +96,7 @@ def create_widget_dynamo(request_dictionary, args):
     # I modified this from a code snippet found here
     # https://medium.com/@NotSoCoolCoder/handling-json-data-for-dynamodb-using-python-6bbd19ee884e
     boto3.resource('dynamodb').Table(args.write_table).put_item(Item=widget)
+    logger.debug(f"Widget saved to dynamodb table {args.write_table}.")
 
 
 def create_widget_s3(client, request_dictionary, args):
@@ -89,6 +108,7 @@ def create_widget_s3(client, request_dictionary, args):
         Bucket=args.write_bucket,
         Key=f"widgets/{owner}/{widget_id}"
     )
+    logger.debug(f"Widget saved to s3 bucket {args.write_bucket}.")
 
 
 if __name__ == '__main__':
