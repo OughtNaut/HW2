f8467527b3b339c8720dbd0e8b668fff15e63b5e
OughtNaut <112574273+OughtNaut@users.noreply.github.com>
2023-11-19 17:35:29 -0700

    Added test body function

--- widgetRequestHandlerTest.py
+++ widgetRequestHandlerTest.py
@@ -20,6 +20,11 @@ class WidgetRequestHandlerTest(unittest.TestCase):
             "body": '{"type":"update","requestId":"840117b1-b4b8-4750-8757-36bc5a441f9e","widgetId":"8123f304-f23f-440b-a6d3-80e979fa4cd6","owner":"Mary Matthews","description":"GEYPOLQOEELYDNNLBCLPQGTNIAGBJMIGGKD","otherAttributes":[{"name":"color","value":"yellow"},{"name":"size","value":"301"},{"name":"size-unit","value":"cm"},{"name":"height","value":"637"},{"name":"height-unit","value":"cm"},{"name":"width","value":"85"},{"name":"rating","value":"0.23102671"},{"name":"price","value":"24.44"},{"name":"quantity","value":"843"},{"name":"vendor","value":"TKDWESJEFIW"},{"name":"note","value":"ZDXTSRLNLKGBPEZOUAQOKQSXONDVUGTOAVAKVMMGJJCMHVSXUNVFWCTKEDPIZROGHEKCIVAWEOMKMWGQCRZJXWAECGNCUCXGKBWNMKGHUDRJPXYQOFNZXYRPYFWXGYKEMJNGAKIHLXHHNOJJIJFFTVGUIVCPJOKOEEJWLDAJDKWMZREXDWTLPJMPOOEASBTZUOCSAZOVCNHOWWMVURWXOHSYMNXBKTBHVWCCUNULSLRNDUTZHKHDNBWTOPRPERHKEUTPOBQAJYSNJXFDDKSWJACWUJIBQFORREFKZIWEVBBIGZUEYPFTCVJQWMVAYXLENZZVYPRBRRXGPAAKLFBDIMNKDDNEBJZVORUVRUUBOLWTAKXJO"}]}',
             "isBase64Encoded": False
         })
+    def get_test_bodies(self):
+        return {
+            "body": '{"type":"update","requestId":"840117b1-b4b8-4750-8757-36bc5a441f9e","widgetId":"8123f304-f23f-440b-a6d3-80e979fa4cd6","owner":"Mary Matthews","description":"GEYPOLQOEELYDNNLBCLPQGTNIAGBJMIGGKD","otherAttributes":[{"name":"color","value":"yellow"},{"name":"size","value":"301"},{"name":"size-unit","value":"cm"},{"name":"height","value":"637"},{"name":"height-unit","value":"cm"},{"name":"width","value":"85"},{"name":"rating","value":"0.23102671"},{"name":"price","value":"24.44"},{"name":"quantity","value":"843"},{"name":"vendor","value":"TKDWESJEFIW"},{"name":"note","value":"ZDXTSRLNLKGBPEZOUAQOKQSXONDVUGTOAVAKVMMGJJCMHVSXUNVFWCTKEDPIZROGHEKCIVAWEOMKMWGQCRZJXWAECGNCUCXGKBWNMKGHUDRJPXYQOFNZXYRPYFWXGYKEMJNGAKIHLXHHNOJJIJFFTVGUIVCPJOKOEEJWLDAJDKWMZREXDWTLPJMPOOEASBTZUOCSAZOVCNHOWWMVURWXOHSYMNXBKTBHVWCCUNULSLRNDUTZHKHDNBWTOPRPERHKEUTPOBQAJYSNJXFDDKSWJACWUJIBQFORREFKZIWEVBBIGZUEYPFTCVJQWMVAYXLENZZVYPRBRRXGPAAKLFBDIMNKDDNEBJZVORUVRUUBOLWTAKXJO"}]}',
+            "badbody": '{"type":"update","requestId":"840117b1-b4b8-4750-8757-36bc5a441f9e","widgetId":"8123f304-f23f-440b-a6d3-80e979fa4cd6","description":"GEYPOLQOEELYDNNLBCLPQGTNIAGBJMIGGKD","otherAttributes":[{"name":"color","value":"yellow"},{"name":"size","value":"301"},{"name":"size-unit","value":"cm"},{"name":"height","value":"637"},{"name":"height-unit","value":"cm"},{"name":"width","value":"85"},{"name":"rating","value":"0.23102671"},{"name":"price","value":"24.44"},{"name":"quantity","value":"843"},{"name":"vendor","value":"TKDWESJEFIW"},{"name":"note","value":"ZDXTSRLNLKGBPEZOUAQOKQSXONDVUGTOAVAKVMMGJJCMHVSXUNVFWCTKEDPIZROGHEKCIVAWEOMKMWGQCRZJXWAECGNCUCXGKBWNMKGHUDRJPXYQOFNZXYRPYFWXGYKEMJNGAKIHLXHHNOJJIJFFTVGUIVCPJOKOEEJWLDAJDKWMZREXDWTLPJMPOOEASBTZUOCSAZOVCNHOWWMVURWXOHSYMNXBKTBHVWCCUNULSLRNDUTZHKHDNBWTOPRPERHKEUTPOBQAJYSNJXFDDKSWJACWUJIBQFORREFKZIWEVBBIGZUEYPFTCVJQWMVAYXLENZZVYPRBRRXGPAAKLFBDIMNKDDNEBJZVORUVRUUBOLWTAKXJO"}]}',
+        }
 
     def test_is_post(self):
         event = {"httpMethod": "POST"}
@@ -47,6 +52,13 @@ class WidgetRequestHandlerTest(unittest.TestCase):
         self.assertIsNone(response)
 
 
+    def test_valid_body(self):
+        test_bodies = self.get_test_bodies()
+        event = {"body": test_bodies["body"]}
+        self.assertTrue(WidgetRequestHandler.valid_body(event))
+        event["body"] = test_bodies["badbody"]
+        self.assertFalse(WidgetRequestHandler.valid_body(event))
 
+        
 if __name__ == '__main__':
     unittest.main()
